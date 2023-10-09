71d2f4622cf7cc28271519050eda0ed42105edf4
Rhys Smoker <r.smoker@gmail.com>
2023-10-09 12:15:30 -0700

    images of books now appear in front end

--- src/components/LendForm.tsx
+++ src/components/LendForm.tsx
@@ -4,13 +4,10 @@ import * as Yup from 'yup';
 import { Button, TextField, Typography, Container, CircularProgress } from '@mui/material';
 import { styled } from '@mui/system';
 
-const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
-
-
 const BookSchema = Yup.object().shape({
-    title: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!'),
+  title: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!'),
 });
 
 const FormContainer = styled(Container)({
@@ -22,19 +19,35 @@ const FormContainer = styled(Container)({
 const LendForm: React.FC = () => {
   const [bookSearch, setBookSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
 
   const handleSearch = (value: string) => {
     if (value) {
-      fetch(`https://www.googleapis.com/books/v1/volumes?q=${value}&AIzaSyAVuBIkRadwQYHLrJaaXo0BP1LnkukkEWA`)
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=${value}&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`)
+
         .then(response => response.json())
         .then(data => setSearchResults(data.items || []))
         .catch(error => console.error(error));
     }
   };
 
+  const handleOwnBookClick = (book: any) => {
+    const title = book.volumeInfo.title;
+    const author = book.volumeInfo.authors && book.volumeInfo.authors.join(', ');
+    const description = book.volumeInfo.description;
+    const imageUrl = book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail;
+    const googleBooksId = book.id;
+
+    // Handle the selected book here
+  };
+
   const renderSearchResults = searchResults.map((book: any) => (
     <div key={book.id}>
       <p>{book.volumeInfo.title} by {book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
+      {book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail && (
+        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+      )}
+      <button onClick={() => handleOwnBookClick(book)}>I Own This Book</button>
     </div>
   ));
 
