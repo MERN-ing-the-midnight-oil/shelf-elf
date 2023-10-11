e49ac07c1452012e2c19fc0e15dd040bc8ef4153
Rhys Smoker <r.smoker@gmail.com>
2023-10-11 05:20:41 -0700

    fixed conditional on token rendering

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
 import MyLendingLibrary from './components/MyLendingLibrary'; 
 import { Typography } from '@mui/material';
-import { AuthProvider } from './context/AuthContext'; // Adjust the path as needed
+import { useAuth } from './context/AuthContext'; // Adjust the path as needed
 
 function App() {
+  const { token } = useAuth(); // Now it should work without error
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,22 +22,26 @@ function App() {
       <img src={process.env.PUBLIC_URL + '/libraries.png'} alt="Library" style={{ width: '90%', margin: '1rem 0' }} />
 
       <main>
-        <AuthProvider>
-          {/* You can conditionally render these based on user navigation or other logic */}
-          <RegisterForm />
-          <hr style={{ margin: '2rem 0' }} /> {/* Add a divider or style as needed */}
-          <LoginForm />
-          {/* Add conditional rendering here to display either login/register form or dashboard based on auth status */}
-          {/* For example, you might check if a token exists and render <Dashboard /> if true */}
-          <Dashboard />
-          <hr style={{ margin: '2rem 0' }} />
-          <h1>My Lending Library:</h1>
-          <MyLendingLibrary />
-        </AuthProvider>
+          {/* Conditional rendering based on auth status */}
+          { !token ? (
+            <>
+              <RegisterForm />
+              <hr style={{ margin: '2rem 0' }} /> {/* Add a divider or style as needed */}
+              <LoginForm />
+            </>
+          ) : (
+            <>
+              <Dashboard />
+              <hr style={{ margin: '2rem 0' }} />
+              <h1>My Lending Library:</h1>
+              <MyLendingLibrary />        
+              <LendForm />
+        <hr style={{ margin: '2rem 0' }} /> 
+            </>
+          )}
       
         {/* The components below might or might not need access to the auth context. Adjust as necessary. */}
-        <LendForm />
-        <hr style={{ margin: '2rem 0' }} /> {/* Add another divider or style as needed */}
+
       </main>
     </div>
   );
