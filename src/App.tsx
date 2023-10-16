f882a7b5c199a3ebd956b0dc22fe5c68bd76ee21
Rhys Smoker <r.smoker@gmail.com>
2023-10-16 11:27:53 -0700

    made a landing page by conditionally rendering header

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,33 @@
 // src/App.tsx
 import './App.css';
-import RegisterForm from './components/RegisterForm'; 
-import LoginForm from './components/LoginForm'; 
+import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
-import { useAuth } from './context/AuthContext'; // Adjust the path as needed
+import LandingHeader from './components/LandingHeader';
+import { useAuth } from './context/AuthContext';
 
 function App() {
-  const { token } = useAuth(); 
+  const { token } = useAuth();
   console.log('Token in App component:', token);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Other's Covers: A Neighborhood Book Sharing App</h1>
-        <h2>Because good pensives make good neighbors.</h2>
-      </header>
-      
-      <img src={process.env.PUBLIC_URL + '/libraries.png'} alt="Library" style={{ width: '90%', margin: '1rem 0' }} />
-
-      <main>
-          {/* Conditional rendering based on auth status */}
-          { !token ? (
-            <>
-              <RegisterForm />
-              <hr style={{ margin: '2rem 0' }} /> {/* Add a divider or style as needed */}
-              <LoginForm />
-            </>
-          ) : (
-            <>
-              <Dashboard />
-              <hr style={{ margin: '2rem 0' }} />
-        <hr style={{ margin: '2rem 0' }} /> 
-            </>
-          )}
-    
-      </main>
+      {!token ? (
+        <>
+          <LandingHeader />
+          <img src={process.env.PUBLIC_URL + '/libraries.png'} alt="Library" style={{ width: '90%', margin: '1rem 0' }} />
+          <RegisterForm />
+          <hr style={{ margin: '2rem 0' }} />
+          <LoginForm />
+        </>
+      ) : (
+        <>
+          <Dashboard />
+          <hr style={{ margin: '2rem 0' }} />
+        </>
+      )}
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/LandingHeader.tsx
@@ -0,0 +1,12 @@
+import '../App.css'; // Import the App.css for styling, assuming it contains the styles for the header.
+
+function LandingHeader() {
+    return (
+        <header className="App-header">
+            <h1>Other's Covers: A Neighborhood Book Sharing App</h1>
+            <h2>Because good pensives make good neighbors.</h2>
+        </header>
+    );
+}
+
+export default LandingHeader;
