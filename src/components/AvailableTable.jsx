8ac2e994b7fdfdda9f31533d9019d0bb7e09b6e9
Rhys Smoker <r.smoker@gmail.com>
2023-11-10 09:52:28 -0800

    refactored with react-table

--- src/components/AvailableTable.jsx
+++ src/components/AvailableTable.jsx
@@ -1,80 +1,61 @@
-import { useMemo } from 'react';
+import React from 'react';
 import { useTable } from 'react-table';
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
 
 const AvailableTable = ({ books, onRequestClick }) => {
-  const data = useMemo(() => books, [books]);
-
-  const columns = useMemo(
+  // Define columns
+  const columns = React.useMemo(
     () => [
-      {
-        Header: 'Title',
-        accessor: 'title', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Author',
-        accessor: 'author',
-      },
-      {
-        Header: 'Description',
-        accessor: 'description',
-      },
-      {
-        Header: 'Offered by',
-        accessor: 'owner.username', // accessing nested properties
-      },
-      {
-        Header: 'Action',
-        accessor: '_id',
-        Cell: ({ row }) => (
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => onRequestClick(row.original)}
-          >
-            Request!
-          </Button>
-        ),
-      },
+      { Header: 'Title', accessor: 'title' },
+      { Header: 'Author', accessor: 'author' },
+      { Header: 'Description', accessor: 'description' },
+      { Header: 'Offered by', accessor: 'owner.username' },
+      // Action column not included here; we will handle it separately
     ],
-    [onRequestClick]
+    []
   );
-  
-  // Use the useTable Hook to send the columns and data to build the table
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data });
 
-  // Render UI for your table
+  // Use the useTable Hook to send the columns and data to build the table
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data: books,
+  });
+
+  // Render the UI for your table
   return (
     <TableContainer component={Paper}>
       <Table {...getTableProps()}>
         <TableHead>
-          {headerGroups.map((headerGroup) => (
+          {headerGroups.map(headerGroup => (
             <TableRow {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
+              {headerGroup.headers.map(column => (
                 <TableCell {...column.getHeaderProps()}>
                   {column.render('Header')}
                 </TableCell>
               ))}
+              <TableCell>Action</TableCell>
             </TableRow>
           ))}
         </TableHead>
         <TableBody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {rows.map(row => {
             prepareRow(row);
             return (
               <TableRow {...row.getRowProps()}>
-                {row.cells.map((cell) => (
+                {row.cells.map(cell => (
                   <TableCell {...cell.getCellProps()}>
                     {cell.render('Cell')}
                   </TableCell>
                 ))}
+                <TableCell align="right">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => onRequestClick(row.original)}
+                  >
+                    Request!
+                  </Button>
+                </TableCell>
               </TableRow>
             );
           })}
