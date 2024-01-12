3c72dca84ce254a3113ffc950a5eae15198c351b
Rhys Smoker <r.smoker@gmail.com>
2024-01-12 13:31:55 -0800

    removed dummy user drop down function

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,11 +1,10 @@
 import React, { useRef, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { Formik, Form, Field, FieldProps, ErrorMessage, useFormikContext } from 'formik';
-import { Typography, Button, TextField, FormControl, Select, MenuItem, InputLabel } from '@mui/material';
+import { Formik, Form, Field, FieldProps, ErrorMessage } from 'formik';
+import { Typography, Button, TextField } from '@mui/material';
 import styled from 'styled-components';
 import axios from 'axios';
 import * as Yup from 'yup';
-import { SelectChangeEvent } from '@mui/material/Select';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
 import Visibility from '@mui/icons-material/Visibility';
@@ -30,32 +29,6 @@ const ErrorText = styled.div`
   margin: 5px 0;
 `;
 
-
-const dummyUsers = [
-  "brownbear1981@example.com",
-  "salmonslayer@example.com",
-  "glacierguider@example.com",
-  "rainforestrover@example.com",
-  "totemcarver@example.com",
-  "midnightsunseeker@example.com",
-  "whalewatcher@example.com",
-  "iceberginnovator@example.com",
-  "fjordfollower@example.com",
-  "ravenreveler@example.com",
-  "pinetreepioneer@example.com",
-  "moosemarauder@example.com",
-  "tundratraveler@example.com",
-  "sitkasprucesavant@example.com",
-  "eagleeyeed@example.com",
-  "northernlightslover@example.com",
-  "ketchikanclimber@example.com",
-  "mendenhallmystic@example.com",
-  "halibuthero@example.com",
-  "bearberrybuddy@example.com",
-];
-
-
-
 const LoginForm: React.FC = () => {
   const { setToken, setUser } = useAuth();
   const [showPassword, setShowPassword] = React.useState(false);
@@ -74,49 +47,13 @@ const LoginForm: React.FC = () => {
     setShowPassword(!showPassword);
   };
 
-  const Dropdown: React.FC = () => {
-    const { setFieldValue } = useFormikContext();
-
-    const handleDropdownChange = (event: SelectChangeEvent) => {
-      const selectedEmail = event.target.value as string;
-      if (selectedEmail) {
-        setFieldValue("email", selectedEmail);
-        setFieldValue("password", "BigBlueBus");
-      }
-    };
-
-
-
-
-    return (
-      <FormControl fullWidth variant="outlined" style={{ marginBottom: '20px' }}>
-        <InputLabel id="select-user-label">Select a user for testing purposes</InputLabel>
-        <Select
-          labelId="select-user-label"
-          onChange={handleDropdownChange}
-          defaultValue=""
-        >
-          <MenuItem value="" disabled>
-            Select a user
-          </MenuItem>
-          {dummyUsers.map(email => (
-            <MenuItem key={email} value={email}>
-              {email}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    );
-  };
-
-
   return (
     <Formik
       initialValues={{ email: '', password: '' }}
       validationSchema={validationSchema}
       onSubmit={async (values, { setSubmitting, setErrors }) => {
         try {
-          const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001';
+          const API_URL = process.env.REACT_APP_BACKEND_URL || '';
           const loginResponse = await axios.post(`${API_URL}/api/users/login`, values);
 
           // Check if the component is still mounted before updating the state
@@ -157,8 +94,6 @@ const LoginForm: React.FC = () => {
         <FormContainer>
           <Form>
             <Typography variant="h5" gutterBottom>Login</Typography>
-
-            <Dropdown />
 
             <Field name="email">
               {({ field, form }: FieldProps) => (
